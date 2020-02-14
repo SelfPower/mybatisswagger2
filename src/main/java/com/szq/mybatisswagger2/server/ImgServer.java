c883142ebb7dafb08fdfd52ce0973d7fd5a92b67
szq <464762143@qq.com>
2020-02-14 17:23:07 +0800

    12

--- src/main/java/com/szq/mybatisswagger2/server/ImgServer.java
+++ src/main/java/com/szq/mybatisswagger2/server/ImgServer.java
@@ -1,7 +1,21 @@
 package com.szq.mybatisswagger2.server;
 
+import com.szq.mybatisswagger2.dao.TbsImgsMapper;
+
+import com.szq.mybatisswagger2.domain.TbsImgs;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+
 public class ImgServer {
 
+    @Autowired
+    private TbsImgsMapper tbsImgsMapper;
+
+    public List<TbsImgs> SelAll(){
+        return tbsImgsMapper.selectAll();
+    }
 
 }
 
