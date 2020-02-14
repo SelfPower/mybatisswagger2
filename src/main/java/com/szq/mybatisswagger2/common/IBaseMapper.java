1ef035d13c0267e5a8e0af94c54355b9ad08a9a4
szq <464762143@qq.com>
2020-02-14 18:40:33 +0800

    Revert "通用mapper加swagger加逆向工程"
    This reverts commit ad546c02

--- src/main/java/com/szq/mybatisswagger2/common/IBaseMapper.java
+++
@@ -1,6 +0,0 @@
-package com.szq.mybatisswagger2.common;
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
-
-public interface IBaseMapper<T> extends Mapper<T>, MySqlMapper<T> {
-}
--- src/main/java/com/szq/mybatisswagger2/dao/TbsImgsMapper.java
+++
@@ -1,7 +0,0 @@
-package com.szq.mybatisswagger2.dao;
-
-import com.szq.mybatisswagger2.common.IBaseMapper;
-import com.szq.mybatisswagger2.domain.TbsImgs;
-
-public interface TbsImgsMapper extends IBaseMapper<TbsImgs> {
-}
